bd2ba6beb8b087e66dedd2afa7deedbe6b4c41b4
Siva-Adabala <sivaadabala21@gmail.com>
2020-04-21 17:53:40 +0530

    backend django with rest_framework, channels and redis

---
+++ notes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from . import models, serializers
+
+class NoteList(generics.ListCreateAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
